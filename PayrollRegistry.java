bcc1d084de1d0805db0d4c1186eb3f7068302d30
Christos Demertzis <picious@gmail.com>
2019-01-12 16:06:33 +0200

    /

--- PayrollRegistry.java
+++ PayrollRegistry.java
@@ -4,12 +4,22 @@ public class PayrollRegistry {
   private String[] employees;
   private int counter;
 
+  /*
+  * Πεδία για την αποθήκευση της διεύθυνσης (πόλη, δρόμος, αριθμός) του υποκαταστήματος
+  * και των υπαλλήλων της (πεδίο τύπου Employee[]). 
+  * Θεωρείστε ότι ένα υποκατάστημα μπορεί να έχει το πολύ 50 υπαλλήλους.
+  */
+  //Constructor
   public PayrollRegistry() {
     employees = new String[50];
     counter = 0;
 
   }
 
+/*
+ * Μέθοδο addEmployee που δέχεται μέσω παραμέτρου ένα αντικείμενο-υπάλληλο 
+ * και τον αποθηκεύει στον πίνακα
+ */
   public void addEmployee(String employee) {
     if (counter < employees.length) {
       employees[counter] = employee;
@@ -21,7 +31,7 @@ public class PayrollRegistry {
    * Μέθοδο updateMothHours που δέχεται τον μήνα και το έτος ως ένα αλφαριθμητικό και διασχίζει τον πίνακα ζητώντας από τον χρήστη να εισάγει από το πληκτρολόγιο τις ώρες εργασίας του κάθε υπαλλήλου εμφανίζοντας το μήνυμα:
    * Δώσε τον αριθμό των ωρών για τον υπάλληλο <όνομα-υπαλλήλου>:
    * και καλώντας τη μέθοδο setHours για την ενημέρωση του αριθμού ωρών εργασίας (ή υπερωρίας).
-  */
+   */
   public void updateMonthHours(Sting year, String month) {
     Scanner input = new Scanner(System.in);
 
